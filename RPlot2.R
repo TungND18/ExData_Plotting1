2162de49d52391581c2804531a73574f63ec68e8
TungND18 <nguyenduytung.ac@gmail.com>
2019-06-27 21:54:27 +0200

    Seperate R code and Plot as png file

---
+++ RPlot2.R
@@ -0,0 +1,5 @@
+png()
+##Generate plot 2
+par(mfrow=c(1,1))
+plot(finalData$SetTime, finalData$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
---
+++ Rplot1.R
@@ -0,0 +1,15 @@
+getwd()
+
+
+householdFile <- "/Users/macbook/Documents/BI /Study/Data analytics/ExData_Plotting1/household_power_consumption.txt"
+
+plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
+## set time variable
+finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(SetTime, finalData)
+##
+## Generating Plot 1
+png()
+hist(finalData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
